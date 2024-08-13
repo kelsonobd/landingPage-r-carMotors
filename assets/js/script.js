0c9080067bef853e7772dc658e9e28f1ad32a9c0
Kelsonobd <kelsonobd@gmail.com>
2024-08-13 18:22:44 -0300

    ajuste final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleciona o ícone do menu e o menu de navegação
+    // Seleciona o ícone do menu, o menu de navegação e os links do menu
     const menuOpener = document.querySelector('.menu-opener');
     const navMenu = document.querySelector('nav');
+    const menuLinks = document.querySelectorAll('.menu li a');
 
     // Adiciona um evento de clique ao ícone do menu
     menuOpener.addEventListener('click', function() {
         // Alterna a classe 'open' no menu de navegação
         navMenu.classList.toggle('open');
+    });
+
+    // Adiciona um evento de clique a cada link do menu
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            // Fecha o menu de navegação ao clicar em qualquer link
+            navMenu.classList.remove('open');
+        });
     });
 });
