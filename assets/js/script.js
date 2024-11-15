ac75ebdc61572530ced999af15d0982e5656c2b7
Kelson Oliveira <kelsonobd@gmail.com>
2024-11-15 11:38:38 -0300

    adicionado funcao modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function openModal(imageUrl) {
+    var modal = document.getElementById("modal");
+    var modalImage = document.getElementById("modal-image");
+    modal.style.display = "block";
+    modalImage.src = imageUrl;
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
